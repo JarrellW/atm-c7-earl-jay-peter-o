5258cb8cf789eba6d0f891360217ddb0634277b3
Jay <73120876+JarrellW@users.noreply.github.com>
2020-11-15 12:42:43 -0500

    remove method works

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -197,7 +197,7 @@ public class Menu {
                     display.closedAccountMenu();
                     if (closeAccountSelection == 1){
                         System.out.println("Thank you for your business");
-//                        accountWarehouse.removeAccount(currentUser, selectedAccount.getAccountNumber());
+                        accountWarehouse.removeAccount(currentUser, selectedAccount.getAccountNumber());
                         loggedInMenu();
                     }
                 }else{
